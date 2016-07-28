b88915267d5a705c2a89b6685d1a55d31d6b481a
Chutnut <Christianbeddoes2@gmail.com>
2016-07-28 20:21:44 +0100

    Add files via upload

---
+++ shared.lua
@@ -0,0 +1,13 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodenitity"
+ENT.Category = "Buildings"
+
+ENT.BaseHealth = 50
+ENT.Model = "models/props_lab/blastdoor001b.mdl"
+
+ENT.PrintName = "Barricade"
+ENT.Author = "Chutnut"
+ENT.Purpose = "Barricade of doors"
+ENT.Instructions = "Place"
+ENT.Spawnable = true
+ENT.AdminSpawnable = false
